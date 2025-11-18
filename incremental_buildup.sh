aceed2a3e8105ce99cb788c2012ac09d4cc8e6fd
root <root@client-0-0.mdl.praxis-pg0.utah.cloudlab.us>
2025-11-18 11:52:50 -0700

    Step 5. Ready to look at performance; parameterized incremental_buildup.sh to take in config

--- incremental_buildup.sh
+++ incremental_buildup.sh
@@ -1,37 +1,28 @@
     # !/bin/bash
 
+    CONFIG="$1"
+
+    if [[ -z "$CONFIG" ]]; then
+        echo "Usage: $0 <path/to/config.json>"
+        exit 1
+    fi
+
     for fanout in 1 2 4 8; do
-        echo "=== Running experiments for client_fanout = $fanout ==="
+        echo "=== Running experiments for client_fanout = $fanout with config = $CONFIG ==="
 
         # Make sure issue_concurrent is set to true
-        sed -i 's/"client_issue_concurrent": false,/"client_issue_concurrent": true,/g' experiments/configs/micro_iocl_2shards_diff_nodes.json
+        sed -i 's/"client_issue_concurrent": false,/"client_issue_concurrent": true,/g' "$CONFIG"
         # Make sure debug is disabled
-        sed -i 's/"client_debug_output": true,/"client_debug_output": false,/g' experiments/configs/micro_iocl_2shards_diff_nodes.json
-        sed -i 's/"server_debug_output": true,/"server_debug_output": false,/g' experiments/configs/micro_iocl_2shards_diff_nodes.json
+        sed -i 's/"client_debug_output": true,/"client_debug_output": false,/g' "$CONFIG"
+        sed -i 's/"server_debug_output": true,/"server_debug_output": false,/g' "$CONFIG"
         # Set client_fanout in JSON
-        sed -i "s/\"client_fanout\": [0-9]\+,/\"client_fanout\": $fanout,/g" experiments/configs/micro_iocl_2shards_diff_nodes.json
+        sed -i "s/\"client_fanout\": [0-9]\+,/\"client_fanout\": $fanout,/g" "$CONFIG"
 
         # ---- Run Experiment ----
-        python3 experiments/run_multiple_experiments.py experiments/configs/micro_iocl_2shards_diff_nodes.json
+        python3 experiments/run_multiple_experiments.py "$CONFIG"
         wait
         # rm -rf experiments/printdbg/2*/2*/2*/out/plots
-        mv experiments/printdbg/2* experiments/printdbg/micro_iocl_2shards_diff_nodes_${fanout}
-        mv experiments/printdbg/micro_iocl_2shards_diff_nodes_${fanout}/plots/tput-p50.png experiments/printdbg/micro_iocl_2shards_diff_nodes_${fanout}/tput-p50.png
-
-        echo "=== Running experiments for client_fanout = $fanout ==="
-
-        # Make sure issue_concurrent is set to true
-        sed -i 's/"client_issue_concurrent": false,/"client_issue_concurrent": true,/g' experiments/configs/micro_iocl_2shards_same_nodes.json
-        # Make sure debug is disabled
-        sed -i 's/"client_debug_output": true,/"client_debug_output": false,/g' experiments/configs/micro_iocl_2shards_same_nodes.json
-        sed -i 's/"server_debug_output": true,/"server_debug_output": false,/g' experiments/configs/micro_iocl_2shards_same_nodes.json
-        # Set client_fanout in JSON
-        sed -i "s/\"client_fanout\": [0-9]\+,/\"client_fanout\": $fanout,/g" experiments/configs/micro_iocl_2shards_same_nodes.json
-
-        # ---- Run Experiment ----
-        python3 experiments/run_multiple_experiments.py experiments/configs/micro_iocl_2shards_same_nodes.json
-        wait
-        # rm -rf experiments/printdbg/2*/2*/2*/out/plots
-        mv experiments/printdbg/2* experiments/printdbg/micro_iocl_2shards_same_nodes_${fanout}
-        mv experiments/printdbg/micro_iocl_2shards_same_nodes_${fanout}/plots/tput-p50.png experiments/printdbg/micro_iocl_2shards_same_nodes_${fanout}/tput-p50.png
+        outdir="experiments/printdbg/micro_$(basename "$CONFIG" .json)_${fanout}"
+        mv experiments/printdbg/2* "$outdir"
+        mv "$outdir/plots/tput-p50.png" "$outdir/tput-p50.png"
     done
