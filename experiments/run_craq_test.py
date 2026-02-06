4e4e575b8d6e4a5da4738bfae4dd67367e651e6f
Alex Eum <alexhyuneum@gmail.com>
2026-02-05 21:14:38 -0700

    Create craq test script

---
+++ experiments/run_craq_test.py
@@ -0,0 +1,13 @@
+from utils.git_util import compile_make
+
+def main():
+    config = {
+        "make_env" : {},
+        "make_clean" : False,
+        "src_directory" : "/users/akalaba/IOCL/src",
+        "make_collect_bins" : ["replication/craq/tests/craq-test"]
+    }
+    compile_make(config)
+
+if __name__ == "__main__":
+    main()
---
+++ experiments/run_craq_test.sh
@@ -0,0 +1,2 @@
+python3 experiments/run_craq_test.py
+./src/bin/craq-test
