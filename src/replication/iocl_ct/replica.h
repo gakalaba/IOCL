eef510ee35fa9e41c11dc369b4e60f722a7d584c
root <root@client-0-0.lanexp.praxis-pg0.wisc.cloudlab.us>
2026-03-30 15:00:37 -0500

    result for no replication and replication with commit

--- src/replication/iocl_ct/replica.h
+++ src/replication/iocl_ct/replica.h
@@ -237,6 +237,8 @@ namespace replication
                                     const proto::DoViewChangeMessage &msg);
             void HandleStartView(const TransportAddress &remote,
                                  const proto::StartViewMessage &msg);
+            void HandlePrepareOKDuplicate(const proto::PrepareOKMessage &msg);
+            void HandleUnorderedPrepareOKDuplicate(const proto::UnorderedPrepareOKMessage &msg);
         };
 
     } // namespace iocl_ct
