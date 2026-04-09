2f98827c4901f1954f49d9fd8bb96d23b11b363f
Alex Eum <alexhyuneum@gmail.com>
2026-04-09 09:52:41 -0500

    Undo fanout changes for vr/craq

--- src/store/benchmark/async/benchmark.cc
+++ src/store/benchmark/async/benchmark.cc
@@ -807,7 +807,7 @@ int main(int argc, char **argv)
             FLAGS_tput_interval,
             FLAGS_abort_backoff, FLAGS_retry_aborted, FLAGS_max_backoff,
             FLAGS_max_attempts,
-            to_issue_concurrent ? FLAGS_client_fanout : 1,
+            FLAGS_client_fanout,
             to_issue_concurrent,
             FLAGS_client_read_percentage,
             FLAGS_wo_replacement);
