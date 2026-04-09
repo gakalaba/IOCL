3b3dfe61f58330145b3624150fc5bc1052a0c488
akalaba <akalaba@us-east-1-2.craq.praxis-pg0.wisc.cloudlab.us>
2026-04-08 23:50:30 -0500

    Fix high vr/craq fanout issue

--- src/store/benchmark/async/benchmark.cc
+++ src/store/benchmark/async/benchmark.cc
@@ -807,7 +807,7 @@ int main(int argc, char **argv)
             FLAGS_tput_interval,
             FLAGS_abort_backoff, FLAGS_retry_aborted, FLAGS_max_backoff,
             FLAGS_max_attempts,
-            FLAGS_client_fanout,
+            to_issue_concurrent ? FLAGS_client_fanout : 1,
             to_issue_concurrent,
             FLAGS_client_read_percentage,
             FLAGS_wo_replacement);
